72a42e504f8260171257b208de6f5d862c6aa2c4
Emrelolrrr <ps265275@summacollege.nl>
2025-09-21 15:03:26 +0200

    Add project files.

---
+++ KorteBroekController/Controllers/KorteBroekController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KorteBroekController.Controllers
+{
+    public class KorteBroekController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Korte Broek?";
+            return View();
+        }
+
+   
+        public IActionResult Weer()
+        {
+
+            var temperatuur = 23; 
+            var regenKans = 10;  
+            var korteBroekAan = temperatuur >= 20 && regenKans < 40;
+
+            ViewData["Title"] = "Weer Vandaag";
+            ViewData["Temperatuur"] = temperatuur;
+            ViewData["RegenKans"] = regenKans;
+            ViewData["KorteBroek"] = korteBroekAan ? "Ja, korte broek kan aan! 😎" : "Nee, beter lange broek. 🧥";
+            ViewData["Afbeelding"] = korteBroekAan ? "/images/sunny.png" : "/images/rainy.png";
+
+            return View();
+        }
+    }
+}
---
+++ KorteBroekController/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KorteBroekController.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
